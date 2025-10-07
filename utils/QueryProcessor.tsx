614ae1c7da301f39837d8d507dd93c25c0e11fc0
Celine Rakab <celinerakab@Celines-MacBook-Air.local>
2025-10-07 10:03:03 -0400

    fixed name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -12,7 +12,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "crakab";
+    return "crakab1";
   }
 
   return "";
